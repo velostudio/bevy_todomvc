fb27fb5ee49666c1e0a58d936766bb8188af2399
tigregalis <anak.harimau@gmail.com>
2023-09-12 22:43:14 +0800

    create todos

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+//! handle_mouse_input: Input<MouseButton> -> Event<TodoAction>
+//! -> update_todo_model: Event<TodoAction> -> (Todo, Model)
+//! ->-> display_todos: (Todo, Model) -> (Text2dBundle, View)
+//! ->-> update_displayed_todos: (Todo, Model) -> (Text2dBundle, View)
+
 use bevy::prelude::*;
 
 fn main() {
@@ -5,13 +10,89 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_event::<TodoAction>()
         .add_systems(Startup, setup)
+        .add_systems(PreUpdate, handle_mouse_input)
+        .add_systems(Update, update_todo_model)
+        .add_systems(Update, display_todos.after(update_todo_model))
+        // .add_systems(Update, update_displayed_todos.after(update_todo_model))
         .run();
 }
 
-fn setup() {
+fn setup(mut commands: Commands) {
     println!("Hello, world!");
+    commands.spawn(Camera2dBundle::default());
 }
 
+fn handle_mouse_input(
+    mut temp: Local<usize>,
+    mouse: Res<Input<MouseButton>>,
+    mut actions: EventWriter<TodoAction>,
+) {
+    if mouse.just_pressed(MouseButton::Left) {
+        println!("new todo");
+        actions.send(TodoAction::Create(format!("new todo {}", *temp)));
+        *temp += 1;
+    }
+}
+
+// TODO: handle ordering of todos
+
+/// Flush after this
+fn update_todo_model(
+    mut commands: Commands,
+    mut actions: EventReader<TodoAction>,
+    mut todos: Query<&mut Todo, ModelOnly>,
+) {
+    for action in actions.iter() {
+        match action {
+            TodoAction::Create(text) => {
+                commands.spawn((
+                    Todo {
+                        text: text.clone(),
+                        checked: false,
+                    },
+                    Model,
+                ));
+            }
+            TodoAction::Delete(e) => {
+                commands.entity(*e).despawn();
+            }
+            TodoAction::UpdateChecked(e, checked) => {
+                todos.get_mut(*e).unwrap().checked = *checked;
+            }
+            TodoAction::UpdateText(e, text) => {
+                todos.get_mut(*e).unwrap().text = text.clone();
+            }
+        }
+    }
+}
+
+fn display_todos(mut commands: Commands, todos: Query<&Todo, (Added<Todo>, ModelOnly)>) {
+    for todo in todos.iter() {
+        commands.spawn((
+            Text2dBundle {
+                text: Text::from_section(&todo.text, default()),
+                ..default()
+            },
+            View,
+        ));
+    }
+}
+
+fn update_displayed_todos(
+    mut commands: Commands,
+    todos: Query<&Todo, (Changed<Todo>, ModelOnly)>,
+    views: Query<&mut Text, ViewOnly>,
+) {
+    for todo in todos.iter() {
+        todo!()
+    }
+}
+
+#[derive(Component)]
+struct Todo {
+    text: String,
+    checked: bool,
+}
 /// Marker component to indicate that this entity is part of the Model
 ///
 /// Mutually exclusive with [`View`]
@@ -36,6 +117,6 @@ type ViewEntity = Entity;
 enum TodoAction {
     Create(String),
     Delete(ModelEntity),
-    UpdateStatus(ModelEntity, bool),
+    UpdateChecked(ModelEntity, bool),
     UpdateText(ModelEntity, String),
 }
