2ef4faebd55e2453236bdaa95e4df1d27ac0015e
tigregalis <anak.harimau@gmail.com>
2023-09-12 20:09:14 +0800

    it begins

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use bevy::prelude::*;
+
 fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup() {
     println!("Hello, world!");
 }
