bdecc4413feae942b336353e971c6ab1e07afc87
tigregalis <anak.harimau@gmail.com>
2023-09-12 20:20:02 +0800

    I think this is what I want

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_event::<TodoAction>()
         .add_systems(Startup, setup)
         .run();
 }
@@ -12,13 +13,29 @@ fn setup() {
 }
 
 /// Marker component to indicate that this entity is part of the Model
+///
+/// Mutually exclusive with [`View`]
 #[derive(Component)]
 struct Model;
 
 /// Marker component to indicate that this entity is part of the View
+///
+/// Mutually exclusive with [`Model`]
 #[derive(Component)]
 struct View;
 
 type ModelOnly = (With<Model>, Without<View>);
 
 type ViewOnly = (Without<Model>, With<View>);
+
+type ModelEntity = Entity;
+
+type ViewEntity = Entity;
+
+#[derive(Event)]
+enum TodoAction {
+    Create(String),
+    Delete(ModelEntity),
+    UpdateStatus(ModelEntity, bool),
+    UpdateText(ModelEntity, String),
+}
