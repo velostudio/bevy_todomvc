b3e3a315fabf84d2ce22bb3f846296c6ceea29ec
tigregalis <anak.harimau@gmail.com>
2023-09-12 20:10:31 +0800

    add marker components

--- src/main.rs
+++ src/main.rs
@@ -10,3 +10,11 @@ fn main() {
 fn setup() {
     println!("Hello, world!");
 }
+
+/// Marker component to indicate that this entity is part of the Model
+#[derive(Component)]
+struct Model;
+
+/// Marker component to indicate that this entity is part of the View
+#[derive(Component)]
+struct View;
